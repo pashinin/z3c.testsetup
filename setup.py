fa5647dc38e552785d1b133bcb27f3b551b261b5
Sergey Pashinin <sergey@pashinin.com>
2014-07-25 11:19:15 +0400

    0.8.7

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ long_description = (
 
 setup(
     name='z3c.testsetup',
-    version='0.8.6',
+    version='0.8.7',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.org',
     url = 'http://pypi.python.org/pypi/z3c.testsetup',
